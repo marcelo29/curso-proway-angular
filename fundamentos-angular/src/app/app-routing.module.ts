5b0ddd051fdef6ed8185a9bcc570a6d6d5b82d8d
marcelo <masasp29@gmail.com>
2025-02-09 18:03:39 -0300

    primeiro-componente

--- fundamentos-angular/src/app/app-routing.module.ts
+++ fundamentos-angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: '',
+      component: PrimeiroComponenteComponent
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fundamentos-angular/src/app/app.component.ts
+++ fundamentos-angular/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   standalone: false,
+  templateUrl: './app.component.html',
+  
   styleUrl: './app.component.css'
 })
 export class AppComponent {
